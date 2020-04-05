1058dec51248a501728d0b899005e0137af0c06e
Puttipong1234 <Puttipong.lims@gmail.com>
2020-04-05 14:54:51 +0700

    first commit

---
+++ FlexMessage/ResultMsg.py
@@ -0,0 +1,433 @@
+def GenerateResultMsg( Profile_name , UserId , Dict_daily_data):
+    
+    msg = {
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "carousel",
+    "contents": [
+      {
+        "type": "bubble",
+        "direction": "ltr",
+        "header": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "image",
+              "url": "https://firebasestorage.googleapis.com/v0/b/pybott-6th.appspot.com/o/doctor (1).png?alt=media&token=74e146ed-e391-4cc6-95cf-6c58fc1032ca"
+            },
+            {
+              "type": "text",
+              "text": "Diagonos Result",
+              "margin": "xl",
+              "size": "lg",
+              "align": "center",
+              "weight": "bold",
+              "color": "#FFFFFF"
+            }
+          ]
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "ข้อมูลผู้ป่วย",
+                      "size": "xl",
+                      "align": "center",
+                      "weight": "bold",
+                      "color": "#3F72AF"
+                    },
+                    {
+                      "type": "text",
+                      "text": "| ชื่อผู้เข้าตรวจ",
+                      "margin": "md",
+                      "weight": "bold",
+                      "color": "#3F72AF"
+                    },
+                    {
+                      "type": "text",
+                      "text": "คุณ " + Profile_name,
+                      "margin": "sm",
+                      "align": "start",
+                      "weight": "bold"
+                    },
+                    {
+                      "type": "text",
+                      "text": "| รหัสผู้ใช้งาน",
+                      "margin": "xl",
+                      "weight": "bold",
+                      "color": "#3F72AF"
+                    },
+                    {
+                      "type": "text",
+                      "text": UserId,
+                      "margin": "sm",
+                      "size": "sm",
+                      "align": "start",
+                      "weight": "bold"
+                    },
+                    {
+                      "type": "text",
+                      "text": "| รายงานวันที่",
+                      "margin": "xl",
+                      "weight": "bold",
+                      "color": "#3F72AF"
+                    },
+                    {
+                      "type": "text",
+                      "text": Dict_daily_data["วันที่"],
+                      "margin": "sm",
+                      "size": "sm",
+                      "align": "start",
+                      "weight": "bold"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "horizontal",
+          "margin": "xl",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "margin": "xxl",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "ขอขอบพระคุณเป็นอย่างสูงที่ให้ความร่วมมือกับน้องหมอนะคะ",
+                  "margin": "xl",
+                  "align": "center",
+                  "weight": "bold",
+                  "color": "#FFFFFF",
+                  "wrap": True
+                }
+              ]
+            }
+          ]
+        },
+        "styles": {
+          "header": {
+            "backgroundColor": "#3F72AF"
+          },
+          "footer": {
+            "backgroundColor": "#3F72AF"
+          }
+        }
+      },
+      {
+        "type": "bubble",
+        "direction": "ltr",
+        "header": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "image",
+              "url": "https://firebasestorage.googleapis.com/v0/b/pybott-6th.appspot.com/o/doctor (1).png?alt=media&token=74e146ed-e391-4cc6-95cf-6c58fc1032ca"
+            },
+            {
+              "type": "text",
+              "text": "Diagonos Result",
+              "margin": "xl",
+              "size": "lg",
+              "align": "center",
+              "weight": "bold",
+              "color": "#FFFFFF"
+            }
+          ]
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "margin": "xxl",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "ข้อมูลอาการ",
+                      "size": "xl",
+                      "align": "center",
+                      "weight": "bold",
+                      "color": "#3F72AF"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "margin": "sm",
+                  "contents": [
+                    {
+                      "type": "box",
+                      "layout": "vertical",
+                      "flex": 7,
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "อาการเฝ้าระวัง",
+                          "size": "md",
+                          "weight": "bold",
+                          "color": "#3F72AF"
+                        },
+                        {
+                          "type": "text",
+                          "text": "มีไข้สูง",
+                          "margin": "lg",
+                          "align": "start",
+                          "weight": "bold"
+                        },
+                        {
+                          "type": "text",
+                          "text": "ไอแห้ง",
+                          "margin": "lg",
+                          "align": "start",
+                          "weight": "bold"
+                        },
+                        {
+                          "type": "text",
+                          "text": "เจ็บคอ",
+                          "margin": "lg",
+                          "align": "start",
+                          "weight": "bold"
+                        },
+                        {
+                          "type": "text",
+                          "text": "เหนื่อยหอบ",
+                          "margin": "lg",
+                          "align": "start",
+                          "weight": "bold"
+                        },
+                        {
+                          "type": "text",
+                          "text": "น้ำมูกไหล",
+                          "margin": "lg",
+                          "align": "start",
+                          "weight": "bold"
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "vertical",
+                      "flex": 4,
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "ระดับ(1-5)",
+                          "size": "md",
+                          "weight": "bold",
+                          "color": "#3F72AF"
+                        },
+                        {
+                          "type": "text",
+                          "text": Dict_daily_data["มีไข้"],
+                          "margin": "lg",
+                          "align": "center",
+                          "weight": "bold"
+                        },
+                        {
+                          "type": "text",
+                          "text": Dict_daily_data["มีอาการไอ"],
+                          "margin": "lg",
+                          "align": "center",
+                          "weight": "bold"
+                        },
+                        {
+                          "type": "text",
+                          "text": Dict_daily_data["มีอาการเจ็บคอ"],
+                          "margin": "lg",
+                          "align": "center",
+                          "weight": "bold"
+                        },
+                        {
+                          "type": "text",
+                          "text": Dict_daily_data["เหนื่อยหอบ"],
+                          "margin": "lg",
+                          "align": "center",
+                          "weight": "bold"
+                        },
+                        {
+                          "type": "text",
+                          "text": Dict_daily_data["น้ำมูกไหล"],
+                          "margin": "lg",
+                          "align": "center",
+                          "weight": "bold"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "type": "text",
+                  "text": "อาการอื่นๆที่พบ",
+                  "margin": "lg",
+                  "weight": "bold",
+                  "color": "#3F72AF"
+                },
+                {
+                  "type": "text",
+                  "text": Dict_daily_data["อาการอื่นๆที่พบ"],
+                  "margin": "md",
+                  "size": "sm",
+                  "align": "end",
+                  "color": "#707070"
+                }
+              ]
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "horizontal",
+          "margin": "xl",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "margin": "xxl",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "ขอขอบพระคุณเป็นอย่างสูงที่ให้ความร่วมมือกับน้องหมอนะคะ",
+                  "margin": "xl",
+                  "align": "center",
+                  "weight": "bold",
+                  "color": "#FFFFFF",
+                  "wrap": True
+                }
+              ]
+            }
+          ]
+        },
+        "styles": {
+          "header": {
+            "backgroundColor": "#3F72AF"
+          },
+          "footer": {
+            "backgroundColor": "#3F72AF"
+          }
+        }
+      },
+      {
+        "type": "bubble",
+        "direction": "ltr",
+        "header": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "image",
+              "url": "https://firebasestorage.googleapis.com/v0/b/pybott-6th.appspot.com/o/doctor (1).png?alt=media&token=74e146ed-e391-4cc6-95cf-6c58fc1032ca"
+            },
+            {
+              "type": "text",
+              "text": "Diagonos Result",
+              "margin": "xl",
+              "size": "lg",
+              "align": "center",
+              "weight": "bold",
+              "color": "#FFFFFF"
+            }
+          ]
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "margin": "xxl",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "ผลการตรวจ",
+                      "size": "xl",
+                      "align": "center",
+                      "weight": "bold",
+                      "color": "#3F72AF"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "margin": "sm",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": Dict_daily_data["ข้อเสนอแนะ"],
+                      "size": "sm",
+                      "align": "center",
+                      "color": "#505050",
+                      "wrap": True
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "horizontal",
+          "margin": "xl",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "margin": "xxl",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "ขอขอบพระคุณเป็นอย่างสูงที่ให้ความร่วมมือกับน้องหมอนะคะ",
+                  "margin": "xl",
+                  "align": "center",
+                  "weight": "bold",
+                  "color": "#FFFFFF",
+                  "wrap": True
+                }
+              ]
+            }
+          ]
+        },
+        "styles": {
+          "header": {
+            "backgroundColor": "#3F72AF"
+          },
+          "footer": {
+            "backgroundColor": "#3F72AF"
+          }
+        }
+      }
+    ]
+  }
+}
+    return msg
---
+++ app.py
@@ -0,0 +1,192 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+from BasicFunction.COVID_ANALYZER import analyze_covid_from_user
+from firebase import firebase
+from BasicFunction.Firebase_Connect import get , get_daily_tracking , post, post_daily_tracking , update_daily_tracking , update , delete
+from config import Firebase_DB_url
+# Firebase_DB_url = "https://pybott-6th.firebaseio.com/" # Your firebase Application
+firebase = firebase.FirebaseApplication(Firebase_DB_url, None)
+DB_COV_TRACKER = "COV_TRACKER"
+DB_USER_SESSION = "USER_SESSION"
+DB_USER_DATA = "USER_DATA"
+
+from config import Channel_access_token , Channel_secret
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(Channel_access_token)
+handler = WebhookHandler(Channel_secret)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # INPUT AND PARSING DATA
+    REPLY_TOKEN = event.reply_token 
+    MESSAGE_FROM_USER = event.message.text
+    UID = event.source.user_id
+    
+    # get user id
+    profile = line_bot_api.get_profile(UID)
+    DISPLAY_NAME = profile.display_name
+    PROFILE_PIC = profile.picture_url
+    
+    #check user in system?
+    user = get(uid=UID,firebase_app=firebase , database_name=DB_USER_DATA)
+    if not user:
+        # continue
+        data = {"session" : "None"}
+        post(uid=UID,data=data,firebase_app=firebase,database_name=DB_USER_SESSION)
+        
+        data = { "DISPLAY_NAME" : DISPLAY_NAME , "PROFILE_PIC" : PROFILE_PIC }
+        post(uid=UID,data=data,firebase_app=firebase,database_name=DB_USER_DATA)
+    
+    
+    user_session = get(uid=UID,firebase_app=firebase , database_name=DB_USER_SESSION)
+    user_session = user_session["session"]
+    if user_session == "None":
+        if MESSAGE_FROM_USER == "เริ่มบันทึกอาการป่วย":
+            daily_report = {
+            "มีไข้" : "",
+            "มีอาการไอ" : "",
+            "มีอาการเจ็บคอ" : "",
+            "น้ำมูกไหล" : "",
+            "เหนื่อยหอบ" : "",
+            "วันที่" : "",
+            "score" : 0,
+            "ข้อเสนอแนะ" : "",
+            "อาการอื่นๆที่พบ": ""
+            }
+            
+            # create user daily report
+            post_daily_tracking(uid=UID , data=daily_report , firebase_app=firebase , database_name=DB_COV_TRACKER)
+            # update session
+            
+            session_data = {"session" : "บันทึกอาการไข้"}
+            update(uid=UID,new_data=session_data,firebase_app=firebase,database_name=DB_USER_SESSION)
+            
+            #Reponse กลับไปที่ห้องแชท
+            line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("ท่านมีอาการไข้สูงหรือไม่คะ? กรุณาระบุระดับความรุนแรงของอาการ (พิมพ์เลข 1-5)"))
+            
+    
+    ### func อื่นๆ
+    else:
+        if  user_session == "บันทึกอาการไข้": # validate session
+                # "3" != 3
+            if MESSAGE_FROM_USER in ["1","2","3","4","5"]: # validate input
+                data = {"มีไข้" : MESSAGE_FROM_USER}
+                update_daily_tracking(uid=UID,new_data=data,firebase_app=firebase,database_name=DB_COV_TRACKER) # update
+                
+                session_data = {"session" : "บันทึกอาการไอ"}
+                update(uid=UID,new_data=session_data,firebase_app=firebase,database_name=DB_USER_SESSION) # update
+                
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("บันทึกอาการไข้เรียบร้อยแล้วคะ มีอาการไอด้วยไหมคะ (พิมพ์เลข 1-5)")) # reponse
+            else :
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("กรุณาระบุเป็นตัวเลขเท่านั้นคะ (พิมพ์เลข 1-5)"))
+        
+        elif  user_session == "บันทึกอาการไอ":
+            if MESSAGE_FROM_USER in ["1","2","3","4","5"]: # validate input
+                data = {"มีอาการไอ" : MESSAGE_FROM_USER}
+                update_daily_tracking(uid=UID,new_data=data,firebase_app=firebase,database_name=DB_COV_TRACKER) # update
+                
+                session_data = {"session" : "บันทึกอาการเจ็บคอ"}
+                update(uid=UID,new_data=session_data,firebase_app=firebase,database_name=DB_USER_SESSION) # update
+                
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("บันทึกอาการไอเรียบร้อยแล้วคะ ต่อไปกรุณาระบุอาการเจ็บคอด้วยคะ (พิมพ์เลข 1-5)")) # reponse
+            else :
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("กรุณาระบุเป็นตัวเลขเท่านั้นคะ (พิมพ์เลข 1-5)"))
+        
+        elif  user_session == "บันทึกอาการเจ็บคอ":
+            if MESSAGE_FROM_USER in ["1","2","3","4","5"]: # validate input
+                data = {"มีอาการเจ็บคอ" : MESSAGE_FROM_USER}
+                update_daily_tracking(uid=UID,new_data=data,firebase_app=firebase,database_name=DB_COV_TRACKER) # update
+                
+                session_data = {"session" : "บันทึกอาการน้ำมูกไหล"}
+                update(uid=UID,new_data=session_data,firebase_app=firebase,database_name=DB_USER_SESSION) # update
+                
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("บันทึกอาการเจ็บคอเรียบร้อยแล้วคะ ต่อไปกรุณาระบุอาการน้ำมูกไหลด้วยคะ (พิมพ์เลข 1-5)")) # reponse
+            else :
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("กรุณาระบุเป็นตัวเลขเท่านั้นคะ (พิมพ์เลข 1-5)"))
+
+        elif  user_session == "บันทึกอาการน้ำมูกไหล":
+            if MESSAGE_FROM_USER in ["1","2","3","4","5"]: # validate input
+                data = {"น้ำมูกไหล" : MESSAGE_FROM_USER}
+                update_daily_tracking(uid=UID,new_data=data,firebase_app=firebase,database_name=DB_COV_TRACKER) # update
+                
+                session_data = {"session" : "บันทึกอาการเหนื่อยหอบ"}
+                update(uid=UID,new_data=session_data,firebase_app=firebase,database_name=DB_USER_SESSION) # update
+                
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("บันทึกอาการอาการน้ำมูกไหลเรียบร้อยแล้วคะ ต่อไปกรุณาระบุอาการเหนื่อยหอบด้วยคะ (พิมพ์เลข 1-5)")) # reponse
+            else :
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("กรุณาระบุเป็นตัวเลขเท่านั้นคะ (พิมพ์เลข 1-5)"))
+
+        elif  user_session == "บันทึกอาการเหนื่อยหอบ":
+            if MESSAGE_FROM_USER in ["1","2","3","4","5"]: # validate input
+                data = {"เหนื่อยหอบ" : MESSAGE_FROM_USER}
+                
+                update_daily_tracking(uid=UID,new_data=data,firebase_app=firebase,database_name=DB_COV_TRACKER) # update
+                
+                session_data = {"session" : "บันทึกอาการอื่นๆ"}
+                update(uid=UID,new_data=session_data,firebase_app=firebase,database_name=DB_USER_SESSION) # update
+                
+                user_daily_data = get_daily_tracking(uid=UID,firebase_app=firebase,database_name=DB_COV_TRACKER)
+                result = analyze_covid_from_user(UID,user_daily_data)
+                
+                post_daily_tracking(uid=UID,data=result,firebase_app=firebase,database_name=DB_COV_TRACKER)
+                
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("บันทึกอาการเหนื่อยหอบเรียบร้อยแล้วคะ ท่านทีอาการอื่นๆเพิ่มเติมอีกไหมคะ บอกน้องหมอได้นะ \n"+ str(result))) # reponse
+            
+            else :
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("กรุณาระบุเป็นตัวเลขเท่านั้นคะ (พิมพ์เลข 1-5)"))
+
+        elif  user_session == "บันทึกอาการอื่นๆ":
+                data = {"อาการอื่นๆที่พบ" : MESSAGE_FROM_USER}
+                
+                update_daily_tracking(uid=UID,new_data=data,firebase_app=firebase,database_name=DB_COV_TRACKER) # update
+                
+                session_data = {"session" : "None"}
+                update(uid=UID,new_data=session_data,firebase_app=firebase,database_name=DB_USER_SESSION) # update
+                
+                user_daily_data = get_daily_tracking(uid=UID,firebase_app=firebase,database_name=DB_COV_TRACKER)
+                result = analyze_covid_from_user(UID,user_daily_data)
+                
+                post_daily_tracking(uid=UID,data=result,firebase_app=firebase,database_name=DB_COV_TRACKER)
+                
+                line_bot_api.reply_message(REPLY_TOKEN,TextSendMessage("บันทึกอาการอื่นๆเรียบร้อยแล้วคะ นี้เป็นผลการตรวจจากการให้ข้อมูลของท่านนะคะ \n"+ str(result))) # reponse
+            
+
+    
+    
+    
+    
+
+
+if __name__ == "__main__":
+    app.run(port=80,debug=True)
