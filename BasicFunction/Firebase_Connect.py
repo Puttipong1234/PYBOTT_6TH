7d8b9a7da4a939559c30898d29060eaddf1094a9
Puttipong1234 <Puttipong.lims@gmail.com>
2020-04-05 11:42:08 +0700

    fix all

--- BasicFunction/Firebase_Connect.py
+++ BasicFunction/Firebase_Connect.py
@@ -1,14 +1,26 @@
+from datetime import date
 # ดึงข้อมูลจาก firebase ตาราง
 def get(uid,firebase_app,database_name,all=False):
     res = firebase_app.get("/"+ database_name , None)
     if not all:
-        return res[uid]
+        return firebase_app.get("/"+ database_name + "/" + uid , None)
     
     else:
         return res
 
+def get_daily_tracking(uid,firebase_app,database_name,all=False):
+    today = date.today()
+    res = firebase_app.get("/"+ database_name + "/"+uid+ "/" + str(today) , None)
+    return res
+
 # สร้างข้อมูล User ใหม่ขึ้นมา
 def post(uid,data,firebase_app,database_name):
+    # data[uid] = {"uid":"4","name":"book4","session":"greeting"}
+    res = firebase_app.patch("/"+database_name+"/"+uid,data)
+    return res
+
+# สร้างข้อมูล User ใหม่ขึ้นมา
+def post_daily_tracking(uid,data,firebase_app,database_name):
     # data[uid] = {"uid":"4","name":"book4","session":"greeting"}
     date = data["วันที่"] # key
     res = firebase_app.patch("/"+database_name+"/"+uid+"/"+date,data)
@@ -17,6 +29,12 @@ def post(uid,data,firebase_app,database_name):
 # อัพเดตข้อมูล User
 def update(uid,new_data,firebase_app,database_name):
     res = firebase_app.patch("/"+database_name+"/"+uid , new_data)
+    return res
+
+# อัพเดตข้อมูล User
+def update_daily_tracking(uid,new_data,firebase_app,database_name,fields):
+    today = date.today()
+    res = firebase_app.patch("/"+database_name+"/"+uid+"/"+str(today)+"/"+fields , new_data)
     return res
 
 # ลบข้อมูล User
--- COVID_APP.py
+++ COVID_APP.py
@@ -1,20 +1,23 @@
 from BasicFunction.COVID_ANALYZER import analyze_covid_from_user
 from firebase import firebase
-from BasicFunction.Firebase_Connect import get , post , update , delete
+from BasicFunction.Firebase_Connect import get , post_daily_tracking , update , delete
 from config import Firebase_DB_url
 # Firebase_DB_url = "https://pybott-6th.firebaseio.com/" # Your firebase Application
 firebase = firebase.FirebaseApplication(Firebase_DB_url, None)
 DB_NAME = "COVID-19-Tracker"
 
-while not input("Press x to Exit!") == "x":
-    print("ยินดีต้องรับเข้าสู่บริการตรวจคัดกรองไวรัส COVID-19 \n คุณควรจะกักตัวหรือไม่")
-    _input_name = input("กรุณากรอกชื่อของท่าน :   ")
-    print("สวัสดีคุณ : " + _input_name)
-    _input_has_fever = input("คุณ" + _input_name+" มีไข้สูงมากกว่า 37.5 องศา หรือไม่? (y/n) :   ")
-    _input_has_cought = input("คุณ" + _input_name+" มีอาการไอหรือไม่ (y/n) :   ")
-    _input_has_เจ็บคอ = input("คุณ" + _input_name+" มีอาการเจ็บคอหรือไม่ (y/n) :   ")
-    _input_has_น้ำมูกไหล = input("คุณ" + _input_name+" มีอาการน้ำมูกไหลหรือไม่ (y/n) :   ")
-    _input_has_เหนื่อยหอบ = input("คุณ" + _input_name+" มีอาการายใจเหนื่อยหอบ หายใจลำบากไหลหรือไม่ (y/n) :   ")
+# while not input("Press x to Exit!") == "x":
+#     print("ยินดีต้องรับเข้าสู่บริการตรวจคัดกรองไวรัส COVID-19 \n คุณควรจะกักตัวหรือไม่")
+#     _input_name = input("กรุณากรอกชื่อของท่าน :   ")
+#     print("สวัสดีคุณ : " + _input_name)
+#     _input_has_fever = input("คุณ" + _input_name+" มีไข้สูงมากกว่า 37.5 องศา หรือไม่? (y/n) :   ")
+#     _input_has_cought = input("คุณ" + _input_name+" มีอาการไอหรือไม่ (y/n) :   ")
+#     _input_has_เจ็บคอ = input("คุณ" + _input_name+" มีอาการเจ็บคอหรือไม่ (y/n) :   ")
+#     _input_has_น้ำมูกไหล = input("คุณ" + _input_name+" มีอาการน้ำมูกไหลหรือไม่ (y/n) :   ")
+#     _input_has_เหนื่อยหอบ = input("คุณ" + _input_name+" มีอาการายใจเหนื่อยหอบ หายใจลำบากไหลหรือไม่ (y/n) :   ")
+
+def COVID_TRACKER_APP(UID , PROFILE_NAME , TEXT_FROM_USER , SESSION):
+
 
     ข้อมูลวันที่1 = {
         "มีไข้" : _input_has_fever,
@@ -30,7 +33,7 @@ while not input("Press x to Exit!") == "x":
     result = analyze_covid_from_user(username=_input_name,dict_report=ข้อมูลวันที่1)
     print(result)
 
-    post(uid=_input_name, data=result, firebase_app=firebase, database_name=DB_NAME) # post ไปที่ firebase
+    post_daily_tracking(uid=_input_name, data=result, firebase_app=firebase, database_name=DB_NAME) # post ไปที่ firebase
 
 
 print("You are Exited!")
