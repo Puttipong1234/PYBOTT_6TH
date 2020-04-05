c63200c7cf610786e2ac67ca5335dfba208bac82
Puttipong1234 <Puttipong.lims@gmail.com>
2020-04-05 11:45:24 +0700

    add

--- BasicFunction/COVID_ANALYZER.py
+++ BasicFunction/COVID_ANALYZER.py
@@ -11,37 +11,31 @@ def analyze_covid_from_user(username,dict_report):
     print("สรุปจากผลการตรวจสอบอาการเบื้องต้น พบว่า")
     score = 0
 
-    if dict_report["มีไข้"] == "y":
-        score += 20
+    if dict_report["มีไข้"]:
+        score += dict_report["มีไข้"]*4
         
     if dict_report["มีอาการไอ"] == "y":
-        score += 20
+        score += dict_report["มีอาการไอ"]*4
         
     if dict_report["น้ำมูกไหล"] == "y":
-        score += 20
+        score += dict_report["น้ำมูกไหล"]*4
 
     if dict_report["มีอาการเจ็บคอ"] == "y":
-        score += 20
+        score += dict_report["มีอาการเจ็บคอ"]*4
 
     if dict_report["เหนื่อยหอบ"] == "y":
-        score += 20
+        score += dict_report["เหนื่อยหอบ"]*4
     
     dict_report["score"] = score
     dict_report["วันที่"] = str(today)
 
-    print("คุณ" + username)
     if 60 <= score < 100:
         dict_report["ข้อเสนอแนะ"] = "ควรกักตัวอยู่ที่บ้านนะคะ"
-        print("ควรกักตัวอยู่ที่บ้านนะคะ")
 
     elif score == 100:
-        print("ควรไปพบแพทย์เดี๋ยวนี้เลยคะ")
         dict_report["ข้อเสนอแนะ"] = "ควรไปพบแพทย์เดี๋ยวนี้เลยคะ"
 
     else :
-        print("ไม่มีอาการสุ่มเสี่ยงต่อเชื่อไวรัส COVID-19")
         dict_report["ข้อเสนอแนะ"] = "ไม่มีอาการสุ่มเสี่ยงต่อเชื่อไวรัส"
-
-    print("ข้อมูลการตรวจเฝ้าระวังวันที่ " + str(today))
     
     return dict_report
